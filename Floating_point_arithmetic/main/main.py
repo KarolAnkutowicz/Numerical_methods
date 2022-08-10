2fe0cc032495276a15ee4d431e35204f6330dbec
Karol Ankutowicz <49668606+KarolAnkutowicz@users.noreply.github.com>
2022-08-10 21:18:20 +0200

    Create function - system linear equations
    Change part of code to create function solving system of two linear equations.

--- Floating_point_arithmetic/main/main.py
+++ Floating_point_arithmetic/main/main.py
@@ -1,3 +1,19 @@
+def rozwiaz_uklad_2(a11, a12, a21, a22, b1, b2) :
+    W = a11 * a22 - a12 * a21
+    Wx = (b1 * a22) - (a12 * b2)
+    Wy = (a11 * b2) - (b1 * a21)
+    if W == 0 :
+        if Wx == 0 and Wy == 0 :
+            print("Uklad ma niekskonczenie wiele rozwiazan...")
+        else :
+            print("Uklad nie ma rozwiazan :(")
+    else :
+        x = Wx / W
+        y = Wy / W
+        print("Rozwiazanie ukladu to")
+        print("x =", x)
+        print("y =", y)
+
 print("Mamy uklad dwoch rownan liniowych:")
 print("a11*x +a12*y = b1")
 print("a21*x +a22*y = b2")
@@ -8,18 +24,4 @@ a21 = float(input("a21 = "))
 a22 = float(input("a22 = "))
 b1 = float(input("b1 = "))
 b2 = float(input("b2 = "))
-
-W = a11 * a22 - a12 * a21
-Wx = (b1 * a22) - (a12 * b2)
-Wy = (a11 * b2) - (b1 * a21)
-if W == 0 :
-    if Wx == 0 and Wy == 0 :
-        print("Uklad ma niekskonczenie wiele rozwiazan...")
-    else :
-        print("Uklad nie ma rozwiazan :(")
-else :
-    x = Wx / W
-    y = Wy / W
-    print("Rozwiazanie ukladu to")
-    print("x =", x)
-    print("y =", y)
+rozwiaz_uklad_2(a11, a12, a21, a22, b1, b2)
